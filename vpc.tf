cec575c25f31d8e041df86789a870b601d050296
Ozzy <ingerul9@gmail.com>
2023-11-21 08:46:34 +0200

    once more the vpc.tf file modified

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = var.VPC_NAME
   cidr = var.VpcCIDR
   azs  = [var.Zone1, var.Zone2, var.Zone3]
-  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSUb3CIDR]
+  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
   enable_nat_gateway = true
